cbfd30abb805d2f589bdd776a8f8c490be2b00ba
Lazaros Laftsis <sdi1700265@di.uoa.gr>
2023-09-21 22:53:09 +0300

    Design apartment page with material-ui

--- src/ApartmentProfile.js
+++ src/ApartmentProfile.js
@@ -2,9 +2,24 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import AuthContext from './AuthContext';
 import { Link } from 'react-router-dom';
+//import './style.css';
 import { MapContainer, TileLayer, Marker, Icon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { Container, Typography, Grid, Paper, Box, Button, CardMedia, Card, Divider, CardContent} from '@material-ui/core';
+import {
+    DateRange as DateRangeIcon,
+    People as PeopleIcon,
+    AttachMoney as AttachMoneyIcon,
+    Rule as RuleIcon,
+    Description as DescriptionIcon,
+    Hotel as HotelIcon,
+    Bathtub as BathtubIcon,
+    MeetingRoom as MeetingRoomIcon,
+    LiveTv as LiveTvIcon,
+    Home as HomeIcon,
+    LocationOn as LocationOnIcon,
+} from '@mui/icons-material';
 
 const icon = new L.Icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
@@ -20,6 +35,7 @@ function ApartmentProfile() {
     const [apartment, setApartment] = useState(null);
     const { user } = useContext(AuthContext);
     const [images, setImages] = useState([]);
+    const [mainImage, setMainImage] = useState('');
 
     useEffect(() => {
         // Fetch apartment details
@@ -58,28 +74,126 @@ function ApartmentProfile() {
     
     }, [apartmentId]);
     
-    
+    const handleImageClick = (imageUrl) => {
+        setMainImage(imageUrl);
+    };
 
     return (
-        <div>
+        <Container>
             {apartment ? (
-                <>
-                    <h2>{apartment.nickname}</h2>
-                    <p>{apartment.type_of_apartment} in {apartment.location}</p>
-                    <p>Open Date: {apartment.open_date}</p>
-                    <p>Close Date: {apartment.close_date}</p>
-                    <p>Number of Guests: {apartment.number_of_guests}</p>
-                    <p>Min Price: {apartment.min_price}</p>
-                    <p>Additional Cost Per Person: {apartment.additional_cost_per_person}</p>
-                    <p>Rules: {apartment.rules}</p>
-                    <p>Description: {apartment.description}</p>
-                    <p>Number of Beds: {apartment.number_of_beds}</p>
-                    <p>Number of Bathrooms: {apartment.number_of_bathrooms}</p>
-                    <p>Number of Rooms: {apartment.number_of_rooms}</p>
-                    <p>Living Room: {apartment.living_room ? "Yes" : "No"}</p>
-                    <p>Square Meters: {apartment.square_meters}</p>
-                    <p>Exact Location: {apartment.exact_location}</p>
-                    <div style={{ height: '300px', width: '100%' }}>
+                <Box mt={4} >
+                    <Typography variant="h4" color="primary">{apartment.nickname}</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">{apartment.type_of_apartment} in {apartment.location}</Typography>
+                    {mainImage && <CardMedia component="img" image={`http://localhost:5000/${mainImage}`}
+                    style={{ height: '400px', width: '100%', objectFit: 'contain', margin: '20px 0' }} />
+                    }
+                    <Box mt={3}>
+                    <Grid container spacing={2}>
+                        {images.map((imageUrl, index) => (
+                            <Grid item xs={4} key={index}>
+                                <Paper elevation={3} onClick={() => handleImageClick(imageUrl)} style={{ cursor: 'pointer' }}>
+                                    <CardMedia component="img" image={`http://localhost:5000/${imageUrl}`} alt="Apartment" style={{ height: '150px' }} />
+                                </Paper>
+                            </Grid>
+                        ))}
+                    </Grid>
+                    </Box>
+                    <Box mt={3}>
+
+                        <Typography variant="h6" gutterBottom>Details:</Typography>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <DateRangeIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Available from:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{new Date(apartment.open_date).toISOString().split('T')[0]}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <DateRangeIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Close Date:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{new Date(apartment.close_date).toISOString().split('T')[0]}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <PeopleIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Number of Guests:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.number_of_guests}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <AttachMoneyIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Starting price:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.min_price}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <AttachMoneyIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Additional Cost Per Person:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.additional_cost_per_person}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <RuleIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Rules:</Typography>
+                        <Typography variant="body1" style={{ marginLeft: 8 }}>{apartment.rules}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <DescriptionIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Description:</Typography>
+                        <Typography variant="body1" style={{ marginLeft: 8 }}>{apartment.description}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <HotelIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Number of Beds:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.number_of_beds}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <BathtubIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Number of Bathrooms:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.number_of_bathrooms}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <MeetingRoomIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Number of Rooms:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.number_of_rooms}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <LiveTvIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Living Room:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.living_room ? "Yes" : "No"}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <HomeIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Square Meters:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.square_meters}</Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <LocationOnIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Exact Location:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.exact_location}</Typography>
+                    </Box>
+                        <Divider variant="middle" />
+                    </Box>
+
+                    <Box mt={4} style={{ height: '300px' }}>
                         <MapContainer
                             center={apartment.exact_location.split(',').map(coord => parseFloat(coord))}
                             zoom={13}
@@ -94,20 +208,29 @@ function ApartmentProfile() {
                                 icon={icon}
                             />
                         </MapContainer>
-                    </div>
-                    <p>Address: {apartment.address}</p>
-                    <p>Nickname: {apartment.nickname}</p>
-                    <h2>Apartment Images</h2>
-                       {images.map((imageUrl, index) => (
-                        <img key={index} src={`http://localhost:5000/${imageUrl}`} alt="Apartment" />
-                        ))}
-                    <h3>Owner's Username: {apartment.username}</h3>
-                    {user?.id === apartment?.owner_id && <Link to={`/edit-apartment/${apartment.id}`}>Edit</Link>}
-                </>
+                    </Box>
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <LocationOnIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Address:</Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.address}</Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" mt={1} mb={1}>
+                        <PeopleIcon color="action" style={{ marginRight: 8 }} />
+                        <Typography variant="subtitle1" color="textSecondary">Owner: </Typography>
+                        <Typography variant="body2" style={{ marginLeft: 8 }}>{apartment.username}</Typography>
+                    </Box>
+
+                    {user?.id === apartment?.owner_id && <Box mt={3}>
+                            <Button variant="contained" color="primary" component={Link} to={`/edit-apartment/${apartment.id}`}>Edit</Button>
+                        </Box>
+                    }
+                </Box>
             ) : (
-                <p>Loading apartment details...</p>
+                <Box mt={4}>
+                    <Typography variant="h6" color="textSecondary">Loading apartment details...</Typography>
+                </Box>
             )}
-        </div>
+        </Container>
     );
     
     
--- src/Header.js
+++ src/Header.js
@@ -2,35 +2,47 @@ import { useContext } from 'react';
 import AuthContext from './AuthContext';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 function Header() {
   const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
-    <header className="header">
-      <h1>Αναζήτηση Διαμερισμάτων</h1>
-      <nav>
-        <ul className="nav-list">
-          <li><Link to="/">Αρχική</Link></li>
-          <li><Link to="/about">Σχετικά με Εμάς</Link></li>
-          <li><Link to="/contact">Επικοινωνία</Link></li>
-          {user ? (
-  <>
-    <li><Link to={`/users/${user.id}`}>{user.username}</Link></li>
-    <li><Link to="/apartment-management">Manage Apartments</Link></li>
-    <li onClick={() => {
-       logout();
-       navigate('/');
-    }}>Logout</li>
-  </>
-) : (
-  <li><Link to="/login">Σύνδεση/Εγγραφή</Link></li>
-)}
+    <AppBar position="static" color="primary" elevation={2} style={{ backgroundColor: '#3f51b5' }}>
+            <Toolbar>
+                <Typography variant="h6" style={{ flexGrow: 1, fontWeight: 500 }}>
+                    Αναζήτηση Διαμερισμάτων
+                </Typography>
 
-        </ul>
-      </nav>
-    </header>
+                <Box mr={2}>
+                    <Button color="inherit" component={Link} to="/">Αρχική</Button>
+                </Box>
+                <Box mr={2}>
+                    <Button color="inherit" component={Link} to="/about">Σχετικά με Εμάς</Button>
+                </Box>
+                <Box mr={2}>
+                    <Button color="inherit" component={Link} to="/contact">Επικοινωνία</Button>
+                </Box>
+
+                {user ? (
+                    <>
+                        <Box mr={2}>
+                            <Button color="inherit" component={Link} to={`/users/${user.id}`}>{user.username}</Button>
+                        </Box>
+                        <Box mr={2}>
+                            <Button color="inherit" component={Link} to="/apartment-management">Manage Apartments</Button>
+                        </Box>
+                        <Button variant="outlined" color="secondary" onClick={() => {
+                            logout();
+                            navigate('/');
+                        }}>Logout</Button>
+                    </>
+                ) : (
+                    <Button color="secondary" component={Link} to="/login">Σύνδεση/Εγγραφή</Button>
+                )}
+            </Toolbar>
+        </AppBar>
   );
 }
 
